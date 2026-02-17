dfc2bd5c61b75259bf774781dc04d09487d18652
Olexiy-repin <repinlyoha@gmail.com>
2026-02-17 21:30:09 +0200

    classwork

---
+++ js/01-vars.js
@@ -0,0 +1,25 @@
+/*
+ * Змінні та типи даних
+ *
+ * - Оголошення змінних з let та const
+ * - Найменування змінних
+ * - Перевизначення значення
+ * - Вибір між const і let
+ * - Виведення даних
+ * - Отримання даних
+ * - Примітивні типи даних: number, string, boolean, null, undefined.
+ * - Оператор typeof
+ */
+
+// let pricePerItem = 1000;
+
+// console.log(pricePerItem);
+
+// pricePerItem = 999;
+
+// console.log(pricePerItem);
+
+// // alert('Hello world!');
+// const firstName = prompt('Введіть ваше імʼя');
+
+// console.log(firstName);
---
+++ js/04-comp.js
@@ -0,0 +1,24 @@
+/*
+ * Оператори
+ *
+ * - Оператори порівняння
+ * - Оператори рівності
+ */
+
+// console.log(5 > 4);
+
+// console.log(6 >= 6);
+
+// console.log(10 > '7');
+
+// console.log('papaya' < 'pap'); // !
+
+// console.log('3' > '21');
+
+// console.log('4' == 4);
+
+// console.log('4' === 4);
+
+// console.log('5' != 5);
+
+// console.log('5' !== 5);
