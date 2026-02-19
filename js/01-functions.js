da303a971aa1c010eb5024f53993a83b63dba5f3
Olexiy-repin <repinlyoha@gmail.com>
2026-02-19 21:42:36 +0200

    classwork

---
+++ js/01-functions.js
@@ -0,0 +1,40 @@
+/*
+ * Функції
+ *
+ * - Оголошення та виклик функції
+ * - Параметри та аргументи
+ * - Повернення значення
+ * - Порядок виконання коду
+ */
+
+// function greetUser(firstName, lastName) {
+//   // let firstName = 'Carl';
+//   // let lastName = 'Rowe';
+
+//   console.log(`Welcome ${firstName} ${lastName}`);
+// }
+
+// greetUser('Oleksii', 'Repin');
+
+// greetUser('Carl', 'Rowe');
+
+// function add(numA, numB) {
+//   const result = numA + numB;
+
+//   return result;
+// }
+
+// console.log(add(10, 20));
+// console.log(add(100, 222));
+
+/*
+TODO: Напишіть функцію getRectArea(sideA, sideB) для обчислення площі прямокутника.
+TODO: Сторони прямокутника будуть переденні до параметрів sideA та sideB.
+*/
+// function getRectArea(sideA, sideB) {
+//   return sideA * sideB;
+// }
+
+// console.log(getRectArea(8, 11)); // 88
+// console.log(getRectArea(18, 11)); // 198
+// console.log(getRectArea(8, 21)); // 168
---
+++ js/05-methods.js
@@ -0,0 +1,16 @@
+/*
+ * Методи рядків
+ *
+ *  - slice
+ *  - toLowerCase/toUpperCase
+ *  - includes
+ *  - startsWith/endsWith
+ *  - indexOf/lastIndexOf
+ *  - trim
+ */
+
+// const email = '  test@gmail.com  ';
+// const result = email.trim();
+
+// console.log(email);
+// console.log(result);
